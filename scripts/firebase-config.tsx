cd4000671b05e3e4cb80dc1f71a77b9ae46b432c
Matheus-Caique <matieves2004@gmail.com>
2026-03-19 20:15:43 -0300

    configurando firebase

---
+++ scripts/firebase-config.tsx
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDn4T30xCtgWXa2uv4xN93AvcTPsq2kIEg",
+  authDomain: "projetos-2026-1---matheusc.firebaseapp.com",
+  projectId: "projetos-2026-1---matheusc",
+  storageBucket: "projetos-2026-1---matheusc.firebasestorage.app",
+  messagingSenderId: "788153572376",
+  appId: "1:788153572376:web:7439a901bca35b6fa8f76d",
+  measurementId: "G-DXZVYE86Y5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export {auth}
